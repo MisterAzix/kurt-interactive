e6484405997eab3c7a00e02031130f6614d5d074
MisterAzix <flizi@live.fr>
2022-02-11 17:28:26 +0100

    :sparkles: Base container

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,9 @@
-import { Animated, Basic, bounce, Combined } from '../shared/styles'
+import { Container } from "../shared/styles";
 
 const Home = () => (
-  <div>
-    <Basic>Cool Styles</Basic>
-    <Combined>
-      With <code>:hover</code>.
-    </Combined>
-    <Animated animation={bounce}>Let's bounce.</Animated>
-  </div>
-)
+    <Container>
+        
+    </Container>
+);
 
-export default Home
+export default Home;
--- shared/styles.js
+++ shared/styles.js
@@ -1,67 +1,28 @@
-import { css, Global, keyframes } from '@emotion/react'
-import styled from '@emotion/styled'
+import { css, Global, keyframes } from "@emotion/react";
+import styled from "@emotion/styled";
 
 export const globalStyles = (
-  <Global
-    styles={css`
-      html,
-      body {
-        padding: 3rem 1rem;
-        margin: 0;
-        background: papayawhip;
-        min-height: 100%;
-        font-family: Helvetica, Arial, sans-serif;
-        font-size: 24px;
-      }
-    `}
-  />
-)
+    <Global
+        styles={css`
+            html,
+            body {
+                display: flex;
+                justify-content: center;
+                margin: 0;
+                font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell",
+                    "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+                -webkit-font-smoothing: antialiased;
+                -moz-osx-font-smoothing: grayscale;
+                background-color: #1a1a1a;
+            }
+        `}
+    />
+);
 
-export const basicStyles = css`
-  background-color: white;
-  color: cornflowerblue;
-  border: 1px solid lightgreen;
-  border-right: none;
-  border-bottom: none;
-  box-shadow: 5px 5px 0 0 lightgreen, 10px 10px 0 0 lightyellow;
-  transition: all 0.1s linear;
-  margin: 3rem 0;
-  padding: 1rem 0.5rem;
-`
-
-export const hoverStyles = css`
-  &:hover {
-    color: white;
-    background-color: lightgray;
-    border-color: aqua;
-    box-shadow: -15px -15px 0 0 aqua, -30px -30px 0 0 cornflowerblue;
-  }
-`
-export const bounce = keyframes`
-  from {
-    transform: scale(1.01);
-  }
-  to {
-    transform: scale(0.99);
-  }
-`
-
-export const Basic = styled.div`
-  ${basicStyles};
-`
-
-export const Combined = styled.div`
-  ${basicStyles};
-  ${hoverStyles};
-  & code {
-    background-color: linen;
-  }
-`
-export const Animated = styled.div`
-  ${basicStyles};
-  ${hoverStyles};
-  & code {
-    background-color: linen;
-  }
-  animation: ${({ animation }) => animation} 0.2s infinite ease-in-out alternate;
-`
+export const Container = styled.div({
+    height: "100vh",
+    width: "100vw",
+    maxWidth: "420px",
+    overflow: "hidden",
+    backgroundColor: "#FEFEF2",
+});
