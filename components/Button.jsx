b53b66f460a6a17ed35edbf5f46f979673058699
MisterAzix <flizi@live.fr>
2022-02-13 02:14:46 +0100

    :sparkles: Button component

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,16 +1,57 @@
+import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 import React from "react";
 
 export default function Button(props) {
-    return <Btn>{props.children}</Btn>;
+    return <Btn layout={props.layout} arrow={props.arrow}>{props.children}</Btn>;
 }
 
-const Btn = styled.button({
-    border: "none",
-    outline: "none",
-    color: "#FFFFFF",
-    background: "#1A1A1A",
-    fontFamily: "Gopher, sans-serif",
-    padding: "1rem 1.5rem",
-    cursor: "pointer",
-});
+const btnBaseStyle = css`
+    border: none;
+    outline: none;
+    font-family: Gopher, sans-serif;
+    font-size: 1.1rem;
+    font-weight: 500;
+    padding: 1rem 1.5rem;
+    border-radius: 4px;
+    cursor: pointer;
+    transition: 0.3s ease;
+`;
+
+const primaryBtn = css`
+    color: #ffffff;
+    background-color: #1a1a1a;
+
+    &:hover {
+        background-color: #333333;
+    }
+`;
+
+const secondaryBtn = css`
+    box-shadow: inset 0 0 0 1px #1a1a1a;
+    color: #1a1a1a;
+    background: none;
+
+    &:hover {
+        box-shadow: inset 0 0 0 2px #1a1a1a;
+    }
+`;
+
+const leftArrow = css`
+    &:before {
+        content: "← ";
+    }
+`;
+
+const rightArrow = css`
+    &:after {
+        content: " →";
+    }
+`;
+
+const Btn = styled.button`
+    ${btnBaseStyle}
+    ${(props) => (props.layout === "secondary" ? secondaryBtn : primaryBtn)}
+    ${(props) => props.arrow === "left" && leftArrow}
+    ${(props) => props.arrow === "right" && rightArrow}
+`;
