d66bfd47ea821f74c9b7975d251309a7f025deae
MisterAzix <flizi@live.fr>
2022-02-13 03:53:06 +0100

    :mag: Adding OpenGraph meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,36 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
     return (
-        <Html>
+        <Html lang="fr">
             <Head>
-                <meta name="description" content="Kurt Interactive is a narrative and interactive experience where you play as Kurt, a 6 year old child who wants to create." />
+                <meta
+                    name="description"
+                    content="Kurt Interactive is a narrative and interactive experience where you play as Kurt, a 6 year old child who wants to create."
+                />
+
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://metatags.io/" />
+                <meta property="og:title" content="Kurt" />
+                <meta
+                    property="og:description"
+                    content="Kurt Interactive is a narrative and interactive experience where you play as Kurt, a 6 year old child who wants to create."
+                />
+                <meta
+                    property="og:image"
+                    content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+                />
+
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:url" content="https://metatags.io/" />
+                <meta property="twitter:title" content="Kurt" />
+                <meta
+                    property="twitter:description"
+                    content="Kurt Interactive is a narrative and interactive experience where you play as Kurt, a 6 year old child who wants to create."
+                />
+                <meta
+                    property="twitter:image"
+                    content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+                />
 
                 <link rel="shortcut icon" href="/favicon.ico" />
                 <link rel="stylesheet" href="https://use.typekit.net/axx7xmq.css" />
