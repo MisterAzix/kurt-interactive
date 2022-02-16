1cb2dacfd30b9594ad00e5ae4b3a292395afa091
FlanB <clement.renou@mmibordeaux.com>
2022-02-16 11:32:09 +0100

    ✨ mother_laughing

---
+++ pages/conclusion.js
@@ -0,0 +1,157 @@
+import { Container } from "../shared/styles"
+import { useState } from "react"
+import styled from "@emotion/styled"
+import PostIt from "../components/PostIt"
+import Button from "../components/Button"
+import { css } from "@emotion/react"
+import Header from "../components/Header"
+import Dialog from "../components/Dialog"
+
+export default function conclusion () {
+    const [slide, setSlide] = useState(1)
+
+    const handleNext = () => {
+        setSlide(slide + 1)
+    }
+
+    switch (slide) {
+        case 1:
+            return <Container>
+                <Grid slide="1">
+                    <Item name="header">
+                        <Header/>
+                    </Item>
+                    <Item name="illustration">
+                        <Illustration src="/illustrations/director.png"/>
+                    </Item>
+                    <Item name="postit">
+                        <PostIt>Une dame s’approche de toi !
+                            C’est la directrice du musée !
+                            Elle regarde attentivement ton dessin...</PostIt>
+                    </Item>
+                    <Item name="button">
+                        <Button onClick={handleNext} arrow="right">Suivant</Button>
+                    </Item>
+                </Grid>
+            </Container>
+        case 2:
+            return <Container>
+                <Grid slide="2">
+                    <Item name="header">
+                        <Header/>
+                    </Item>
+                    <Item name="illustration">
+                        <Illustration src="/illustrations/mother_looking_draw.png"/>
+                    </Item>
+                    <Item name="postit">
+                        <PostIt>Ta maman vous rejoint. Elle aussi regarde ton dessin. Elle est intriguée.</PostIt>
+                    </Item>
+                    <Item name="button">
+                        <Button onClick={handleNext} arrow="right">Suivant</Button>
+                    </Item>
+                </Grid>
+            </Container>
+        case 3:
+            return <Container>
+                <Grid slide="3">
+                    <Item name="header">
+                        <Header/>
+                    </Item>
+                    <Item name="illustration">
+                        <Illustration style={{ width: "100%" }} src="/illustrations/mother&director.png"/>
+                    </Item>
+                    <Item name="postit">
+                        <PostIt>La directrice du musée parle à ta mère.</PostIt>
+                    </Item>
+                    <Item name="dialog">
+                        <Dialog name="Directrice du Musée :">
+                            Je trouve que le dessin de votre fils est très créatif, vous devriez laisser s’exprimer sa
+                            créativité, car dans l’art il y a aussi de l’amusement !
+                        </Dialog>
+                    </Item>
+                    <Item name="button">
+                        <Button onClick={handleNext} arrow="right">Suivant</Button>
+                    </Item>
+                </Grid>
+            </Container>
+        case 4:
+            return <Container>
+                <Grid slide="4">
+                    <Item name="header">
+                        <Header/>
+                    </Item>
+                    <Item name="illustration">
+                        <Illustration src="/illustrations/mother_laughing.png"/>
+                    </Item>
+                    <Item name="postit">
+                        <PostIt>Je n’avais pas vu cela sous cet angle ! Kurt, tu es très créatif, je suis fier de toi !</PostIt>
+                    </Item>
+                    <Item name="button">
+                        <Button onClick={handleNext} arrow="right">Suivant</Button>
+                    </Item>
+                </Grid>
+            </Container>
+    }
+}
+
+const Illustration = styled.img`
+   width: 75%;
+  `
+
+const Grid = styled.div`
+    display: grid;
+    width: 100%;
+    height: 100%;
+    text-align: center;
+    grid-template-columns: 1fr;
+    ${(props) => {
+    switch (props.slide) {
+        case "3":
+            return css`
+                grid-template-rows: min-content 1fr min-content min-content 20vh;
+                grid-template-areas:
+                        "header"
+                        "illustration"
+                        "postit"
+                        "dialog"
+                        "button";
+            `
+        default:
+            return css`
+                     grid-template-rows: min-content 1fr auto 20vh;
+                    grid-template-areas:
+                        "header"
+                        "illustration"
+                        "postit"
+                        "button";
+                `
+    }
+}}
+`
+
+const Item = styled.div`
+    position: relative;
+    grid-area: ${(props) => props.name};
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    ${(props) => {
+    switch (props.name) {
+        case "button":
+            return css`
+                justify-content: center;
+                `
+        case "postit":
+            return css`
+               top: -24px;
+               margin-bottom: 1rem;
+                `
+        case "illustration":
+            return css`
+                justify-content: flex-end;
+                `
+    }
+}
+
+}
+`
