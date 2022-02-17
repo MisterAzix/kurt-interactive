49ac9e26b1785b9d33fb37eb6a20216617fb1e5e
FlanB <clement.renou@mmibordeaux.com>
2022-02-17 12:13:39 +0100

    💫 kurt animation

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,23 @@
-import styled from "@emotion/styled";
+import styled from "@emotion/styled"
+import { Player } from "@lottiefiles/react-lottie-player"
+import kurt from "../lib/lotties/kurt.json"
 
-export default function Header() {
-    return <HeaderElement>Kurt</HeaderElement>;
+export default function Header () {
+    return <HeaderElement
+        src={kurt}
+        autoplay
+        loop
+        style={{
+            width: "4rem",
+            transform: "translateX(10px)",
+            margin: "1rem 0"
+        }}
+    />
 }
 
-const HeaderElement = styled.header`
+const HeaderElement = styled(Player)`
     font-family: "HeartVibes";
     text-align: center;
     font-size: 1.5rem;
     margin: 2rem 0;
-`;
+`
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,33 @@
-import styled from "@emotion/styled";
-import { Container } from "../shared/styles";
-import Link from "next/link";
+import styled from "@emotion/styled"
+import { Container } from "../shared/styles"
+import Link from "next/link"
 
-import Button from "../components/Button";
-import Bike from "../drawings/Bike";
-import Bird from "../drawings/Bird";
-import Cloud from "../drawings/Cloud";
-import House from "../drawings/House";
-import Sun from "../drawings/Sun";
-import Flower from "../drawings/Flower";
-import Zigzag from "../drawings/Zigzag";
-import Spring from "../drawings/Spring";
-import Circle from "../drawings/Circle";
-import Star from "../drawings/Star";
+import Button from "../components/Button"
+import Bike from "../drawings/Bike"
+import Bird from "../drawings/Bird"
+import Cloud from "../drawings/Cloud"
+import House from "../drawings/House"
+import Sun from "../drawings/Sun"
+import Flower from "../drawings/Flower"
+import Zigzag from "../drawings/Zigzag"
+import Spring from "../drawings/Spring"
+import Circle from "../drawings/Circle"
+import Star from "../drawings/Star"
+import kurt from "../lib/lotties/kurt.json"
+import { Player } from "@lottiefiles/react-lottie-player"
 
 const Home = () => (
     <Container>
         <Grid>
             <Item name="title">
-                <Title>kurt</Title>
+                <Player src={kurt}
+                        autoplay
+                        loop
+                        style={{
+                            width: "8rem",
+                            transform: "translateX(16px)"
+                        }}
+                />
             </Item>
             <Item name="button">
                 <Link href="/introduction">
@@ -29,24 +38,24 @@ const Home = () => (
                 </Link>
             </Item>
         </Grid>
-        <Sun top="5" right="10" />
-        <Cloud top="15" left="10" />
-        <Bird top="35" right="10" />
-        <Bike bottom="35" right="10" />
-        <House bottom="30" left="10" />
-        <Flower bottom="3" right="15" />
-        <Zigzag top="35" left="20" />
-        <Spring bottom="37" left="50" />
-        <Circle bottom="5" left="15" />
-        <Circle top="30" left="5" />
-        <Circle top="42" right="38" />
-        <Star top="48" left="20" />
-        <Star bottom="13" left="25" />
-        <Star bottom="25" right="15" />
+        <Sun top="5" right="10"/>
+        <Cloud top="15" left="10"/>
+        <Bird top="35" right="10"/>
+        <Bike bottom="35" right="10"/>
+        <House bottom="30" left="10"/>
+        <Flower bottom="3" right="15"/>
+        <Zigzag top="35" left="20"/>
+        <Spring bottom="37" left="50"/>
+        <Circle bottom="5" left="15"/>
+        <Circle top="30" left="5"/>
+        <Circle top="42" right="38"/>
+        <Star top="48" left="20"/>
+        <Star bottom="13" left="25"/>
+        <Star bottom="25" right="15"/>
     </Container>
-);
+)
 
-export default Home;
+export default Home
 
 const Grid = styled.div`
     display: grid;
@@ -59,7 +68,7 @@ const Grid = styled.div`
         "."
         "title"
         "button";
-`;
+`
 
 const Item = styled.div`
     grid-area: ${(props) => props.name};
@@ -68,10 +77,4 @@ const Item = styled.div`
     justify-content: center;
     align-items: center;
     gap: 1rem;
-`;
-
-const Title = styled.h1`
-    grid-area: title;
-    font-family: "HeartVibes";
-    font-size: 4.5rem;
-`;
+`
