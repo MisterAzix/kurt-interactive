3573712b0d3ce07fc8f46c1a66e95979ee820aa1
MisterAzix <flizi@live.fr>
2022-03-15 23:12:40 +0100

    :mag: Real GA4 fix

--- pages/_app.js
+++ pages/_app.js
@@ -2,17 +2,17 @@ import { globalStyles } from "../shared/styles";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import * as gtag from "../lib/gtag";
 
 import InventoryWrapper from "../lib/context";
+import Script from "next/script";
 
 const App = ({ Component, pageProps }) => {
     const router = useRouter();
 
     useEffect(() => {
         const handleRouteChange = (url) => {
-            window.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
-                page_path: url,
-            });
+            gtag.pageview(url);
         };
         router.events.on("routeChangeComplete", handleRouteChange);
         return () => {
@@ -57,6 +57,25 @@ const App = ({ Component, pageProps }) => {
                     </>
                 )}
             </Head>
+            {/* Global Site Tag (gtag.js) - Google Analytics */}
+            <Script
+                strategy="afterInteractive"
+                src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GOOGLE_ANALYTICS_ID}`}
+            />
+            <Script
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', '${gtag.GOOGLE_ANALYTICS_ID}', {
+                            page_path: window.location.pathname,
+                            });
+                        `,
+                }}
+            />
+
             {globalStyles}
             <InventoryWrapper>
                 <Component {...pageProps} />
--- pages/_document.js
+++ pages/_document.js
@@ -1,25 +1,9 @@
 import { Html, Head, Main, NextScript } from "next/document";
-/* import Script from "next/script"; */
 
 export default function Document() {
     return (
         <Html lang="fr">
             <Head>
-                <script
-                    async
-                    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-                />
-                <script
-                    dangerouslySetInnerHTML={{
-                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                        })(window,document,'script','dataLayer',${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});`,
-                    }}
-                    /* strategy="worker" */
-                ></script>
-
                 <link rel="shortcut icon" href="/favicon.ico" />
                 <link rel="stylesheet" href="https://use.typekit.net/axx7xmq.css" />
             </Head>
