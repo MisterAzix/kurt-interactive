4b289481512f97515ee99e0c463abbaeee78383c
MisterAzix <flizi@live.fr>
2022-03-15 21:49:27 +0100

    :mag: Fix Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,50 +1,67 @@
 import { globalStyles } from "../shared/styles";
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 import InventoryWrapper from "../lib/context";
 
-const App = ({ Component, pageProps }) => (
-    <>
-        <Head>
-            <title>Kurt</title>
-            <meta
-                name="description"
-                content="Kurt Interactive est une expérience narrative et interactive dans laquelle vous incarnez Kurt, un enfant de 6 ans qui veut créer et devenir un artiste, mais sa mère ne voit pas les choses comme ça et lui dit que cela demande beaucoup de connaissances et d'être une bonne personne."
-            />
+const App = ({ Component, pageProps }) => {
+    const router = useRouter();
 
-            {(Component.name !== "Credits" && Component.name !== "Galery") && (
-                <>
-                    <meta property="og:type" content="website" />
-                    <meta property="og:url" content="kurt-interactive.vercel.app/" />
-                    <meta property="og:title" content="Kurt" />
-                    <meta
-                        property="og:description"
-                        content="Kurt Interactive est une expérience narrative et interactive dans laquelle vous incarnez Kurt, un enfant de 6 ans qui veut créer et devenir un artiste, mais sa mère ne voit pas les choses comme ça et lui dit que cela demande beaucoup de connaissances et d'être une bonne personne."
-                    />
-                    <meta
-                        property="og:image"
-                        content="https://res.cloudinary.com/hf10ee93o/image/upload/v1644720660/Meta_image_evc8hd.jpg"
-                    />
+    useEffect(() => {
+        const handleRouteChange = (url) => {
+            window.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+                page_path: url,
+            });
+        };
+        router.events.on("routeChangeComplete", handleRouteChange);
+        return () => {
+            router.events.off("routeChangeComplete", handleRouteChange);
+        };
+    }, [router.events]);
 
-                    <meta property="twitter:card" content="summary_large_image" />
-                    <meta property="twitter:url" content="kurt-interactive.vercel.app/" />
-                    <meta property="twitter:title" content="Kurt" />
-                    <meta
-                        property="twitter:description"
-                        content="Kurt Interactive est une expérience narrative et interactive dans laquelle vous incarnez Kurt, un enfant de 6 ans qui veut créer et devenir un artiste, mais sa mère ne voit pas les choses comme ça et lui dit que cela demande beaucoup de connaissances et d'être une bonne personne."
-                    />
-                    <meta
-                        property="twitter:image"
-                        content="https://res.cloudinary.com/hf10ee93o/image/upload/v1644720660/Meta_image_evc8hd.jpg"
-                    />
-                </>
-            )}
-        </Head>
-        {globalStyles}
-        <InventoryWrapper>
-            <Component {...pageProps} />
-        </InventoryWrapper>
-    </>
-);
+    return (
+        <>
+            <Head>
+                <title>Kurt</title>
+                <meta
+                    name="description"
+                    content="Kurt Interactive est une expérience narrative et interactive dans laquelle vous incarnez Kurt, un enfant de 6 ans qui veut créer et devenir un artiste, mais sa mère ne voit pas les choses comme ça et lui dit que cela demande beaucoup de connaissances et d'être une bonne personne."
+                />
+
+                {Component.name !== "Credits" && Component.name !== "Galery" && (
+                    <>
+                        <meta property="og:type" content="website" />
+                        <meta property="og:url" content="kurt-interactive.vercel.app/" />
+                        <meta property="og:title" content="Kurt" />
+                        <meta
+                            property="og:description"
+                            content="Kurt Interactive est une expérience narrative et interactive dans laquelle vous incarnez Kurt, un enfant de 6 ans qui veut créer et devenir un artiste, mais sa mère ne voit pas les choses comme ça et lui dit que cela demande beaucoup de connaissances et d'être une bonne personne."
+                        />
+                        <meta
+                            property="og:image"
+                            content="https://res.cloudinary.com/hf10ee93o/image/upload/v1644720660/Meta_image_evc8hd.jpg"
+                        />
+
+                        <meta property="twitter:card" content="summary_large_image" />
+                        <meta property="twitter:url" content="kurt-interactive.vercel.app/" />
+                        <meta property="twitter:title" content="Kurt" />
+                        <meta
+                            property="twitter:description"
+                            content="Kurt Interactive est une expérience narrative et interactive dans laquelle vous incarnez Kurt, un enfant de 6 ans qui veut créer et devenir un artiste, mais sa mère ne voit pas les choses comme ça et lui dit que cela demande beaucoup de connaissances et d'être une bonne personne."
+                        />
+                        <meta
+                            property="twitter:image"
+                            content="https://res.cloudinary.com/hf10ee93o/image/upload/v1644720660/Meta_image_evc8hd.jpg"
+                        />
+                    </>
+                )}
+            </Head>
+            {globalStyles}
+            <InventoryWrapper>
+                <Component {...pageProps} />
+            </InventoryWrapper>
+        </>
+    );
+};
 
 export default App;
--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,7 @@ export default function Document() {
                         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                        })(window,document,'script','dataLayer','GTM-WVR85XM');`,
+                        })(window,document,'script','dataLayer',${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});`,
                     }}
                 ></Script>
 
@@ -22,8 +22,7 @@ export default function Document() {
             <body>
                 <noscript
                     dangerouslySetInnerHTML={{
-                        __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WVR85XM" 
-                        height="0" width="0" style="display:none;visibility:hidden" ></iframe>`,
+                        __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
                     }}
                 ></noscript>
                 <Main />
